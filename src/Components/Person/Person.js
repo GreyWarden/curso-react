9442a3814cd68b5c8bb97735bd5b8ed9c4d39a93
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-16 19:20:59 +0200

    Added Styled component to create the card

--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Person.css'
-
+import styled from 'styled-components'
 const person = (
     {
       name,
@@ -10,12 +10,30 @@ const person = (
       onChangeHandler
     }
 ) => (
-    <div className='Person'>
+    <Card>
       <h2 style={styles} onClick={onClickHandler}>
         My name is {name}, and I'm {age} years old
       </h2>
       <input type="text" value={name} onChange={onChangeHandler}/>
-    </div>
+    </Card>
 );
 
+const Card = styled.div`
+  width: 60%;
+  min-width: 200px;
+  max-width: 200px;
+  margin: 1em;
+  border-radius: 0.8em;
+  border: 1px solid #123456;
+  box-shadow: 0 2px 3px #654321;
+  padding: 1em;
+  text-align: center;
+  flex: 1;
+  :hover {
+      transform: scale(1.7);
+      border-radius: 1em;
+      background-color: white;
+      z-index: 99;
+  }
+`
 export default person;
